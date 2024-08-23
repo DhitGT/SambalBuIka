d6c598fa7d47a61212ddaf5c9fadfad16ffc6833
DhitGT <adityadwisaputra666@gmail.com>
2024-08-23 20:34:51 +0700

    push

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="bg-red-400">
     <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto
-      reprehenderit consequatur minima quae ipsum, veniam facilis magni error
-      voluptas impedit?
+      L
     </p>
   </div>
 </template>
